61ec48e935f50b240f5a62d7a8f469cd46910b98
長岡聡太 <nagaokasota@nagaokasatoshifutoshinoMacBook-Pro.local>
2023-06-14 21:05:47 +0900

    initial commit

---
+++ Assets/Scripts/BattleManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 戦いを管理する
+// PlayerとEnemyを戦わせる
+public class BattleManager : MonoBehaviour
+{
+    // Playerを取得する
+    // Enemyを取得する
+    void Start()
+    {
+
+
+    }
+    // PlayerがEnemyに攻撃する
+    // EnemyがPlayerに攻撃する
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/UnitManager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// キャラクターを管理する
+public class UnitManager : MonoBehaviour
+{
+    // ステータス
+    public int hp;
+    public int pt;
+}
